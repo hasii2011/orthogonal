74bb10b56c4e451a205473378210c825878d45a5
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-09-20 14:42:55 -0500

    <>[]: <Repository was renamed>
    [
    * Check CircleCI build
    * Check updated README
    * Don't need these scripts
    * Make unit test quiet
    ]
    
    [https://github.com/hasii2011/orthogonal/issues/16]

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="orthogonal",
-    version="1.1.8",
+    name='orthogonal',
+    version='1.2.0',
     description="Orthogonal Graph Layout for Python 3",
     long_description=README,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     maintainer='Humberto A. Sanchez II',
     maintainer_email='humberto.a.sanchez.ii@gmail.com',
-    url="https://github.com/hasii2011/OrthogonalDrawing",
+    url='https://github.com/hasii2011/orthogonal',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['networkx==3.0']
