4ea2fb4abbc053a637c48d1e0f69a0a3755d144c
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-09-21 15:52:52 -0500

    <>[]: <Easy Versions>
    []
    
    [https://github.com/hasii2011/orthogonal/issues/17]

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+
 import pathlib
+
 from setuptools import setup
 from setuptools import find_packages
 
+from orthogonal import __version__
+
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
-
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
 setup(
     name='orthogonal',
-    version='1.2.0',
+    version=__version__,
     description="Orthogonal Graph Layout for Python 3",
     long_description=README,
     long_description_content_type='text/markdown',
